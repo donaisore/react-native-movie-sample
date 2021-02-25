b34986ebbcbe8f0b647bb22df99c5cb8b5cf8cce
donaisore <r.h.kobe0706@gmail.com>
2021-02-26 07:28:53 +0900

    絶対パスでimport出来るように

--- babel.config.js
+++ babel.config.js
@@ -1,6 +1,22 @@
-module.exports = function(api) {
+module.exports = function (api) {
   api.cache(true);
   return {
     presets: ['babel-preset-expo'],
+    plugins: [
+      [
+        'module-resolver',
+        {
+          alias: {
+            assets: './assets',
+            src: './src',
+            types: './src/types',
+            components: './src/components',
+            screens: './src/screens',
+            contexts: './src/contexts',
+            navigation: './src/navigation',
+          },
+        },
+      ],
+    ],
   };
 };
