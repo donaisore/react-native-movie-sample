a392c9b357de403e94a2bdbb87bf783edc3dd533
donaisore <r.h.kobe0706@gmail.com>
2021-03-08 09:15:08 +0900

    upload movie.

--- src/screens/MainBottomTab/MyPageScreen.tsx
+++ src/screens/MainBottomTab/MyPageScreen.tsx
@@ -1,12 +1,39 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { FAB } from 'react-native-paper';
 import { SafeAreaView, Text, StyleSheet } from 'react-native';
 
+import firebase from 'src/config/firebase';
+import * as ImagePicker from 'expo-image-picker';
+
 const MyPageScreen = () => {
+  // const [video, setVideo] = useState<ImagePicker.ImagePickerResult | null>(
+  //   null
+  // );
+
+  const openImagePickerAsync = async () => {
+    let permissionResult = await ImagePicker.requestMediaLibraryPermissionsAsync();
+    if (permissionResult.granted === false) {
+      alert('Permission to access camera roll is required!');
+      return;
+    }
+
+    let pickerResult = await ImagePicker.launchImageLibraryAsync({
+      mediaTypes: ImagePicker.MediaTypeOptions.Videos,
+    });
+    if (!pickerResult.cancelled) {
+      const { uri } = pickerResult;
+      const fileName = uri.split('/').slice(-1)[0];
+      const response = await fetch(uri);
+      const blob = await response.blob();
+      const storageRef = firebase.storage().ref('movies/' + fileName);
+      await storageRef.put(blob);
+    }
+  };
+
   return (
     <SafeAreaView style={styles.container}>
       <Text>MyPageScreen</Text>
-      <FAB style={styles.fab} icon='plus' />
+      <FAB style={styles.fab} icon='plus' onPress={openImagePickerAsync} />
     </SafeAreaView>
   );
 };
