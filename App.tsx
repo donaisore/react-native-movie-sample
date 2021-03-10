c004bdb757678d6ee1a3d13aad5136daa0a4a878
donaisore <r.h.kobe0706@gmail.com>
2021-03-10 09:54:39 +0900

    warningを無視

--- App.tsx
+++ App.tsx
@@ -4,9 +4,12 @@ import { UserContext } from 'src/contexts/userContext';
 import AppNavigator from 'src/navigation/AppNavigator';
 import { Provider as PaperProvider } from 'react-native-paper';
 import { User } from 'src/types/user';
+import { LogBox } from 'react-native';
 
 export default function App() {
   const [user, setUser] = useState<User | null>(null);
+
+  LogBox.ignoreLogs(['Setting a timer']);
 
   return (
     <UserContext.Provider value={{ user, setUser }}>
